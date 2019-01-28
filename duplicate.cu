36e831012dec59cf4bf28442d1e9fe06726a86f6
mohadeseh <mohadeseh.f75@gmail.com>
2019-01-28 05:19:14 +0330

    changes

--- duplicate.cu
+++ duplicate.cu
@@ -206,27 +206,27 @@ int main(int argc, char *argv[]){
 	
 	set_clock();
 
-	sequential_duplicate(output_h,input_h, input_size);
+	// sequential_duplicate(output_h,input_h, input_size);
 
-	// for(int counter = 0; counter < img_num; counter++){
-	// 	int j = (counter + 1) * input_size;
-	// 	for(int repeat = 0  ; repeat < img_num - (counter + 1); repeat++){
-	// 		int num_of_one=0;
-	// 		for (int i = counter*input_size; i < (counter+1) *input_size; i++){
-	// 			printf("img_in[i] is %d\n",input_h[i] );
-	// 			printf("img_in[j] is %d\n",input_h[j] );
-	// 			int diff = abs (input_h[i] - input_h[j]);
-	// 			if (diff == 0)
-	// 				num_of_one++;
-	// 			j++;
-	// 		}
-	// 		printf("%d\n",num_of_one );
-	// 		int persent = (num_of_one*100)/input_size;
-	// 		printf("persent is %d\n", persent );
-	// 		output_h[(counter*input_size)+((counter+1)+repeat)]= persent;
-	// 		printf("darsad tashabohe axe %d ba axe %d hast %d \n", counter , counter+repeat+1 , output_h[(counter*input_size)+((counter+1)+repeat)]);
-	// 	}
-	// }
+	for(int counter = 0; counter < img_num; counter++){
+		int j = (counter + 1) * input_size;
+		for(int repeat = 0  ; repeat < img_num - (counter + 1); repeat++){
+			int num_of_one=0;
+			for (int i = counter*input_size; i < (counter+1) *input_size; i++){
+				printf("img_in[i] is %d\n",input_h[i] );
+				printf("img_in[j] is %d\n",input_h[j] );
+				int diff = abs (input_h[i] - input_h[j]);
+				if (diff == 0)
+					num_of_one++;
+				j++;
+			}
+			printf("%d\n",num_of_one );
+			int persent = (num_of_one*100)/input_size;
+			printf("persent is %d\n", persent );
+			output_h[(counter*input_size)+((counter+1)+repeat)]= persent;
+			printf("darsad tashabohe axe %d ba axe %d hast %d \n", counter , counter+repeat+1 , output_h[(counter*input_size)+((counter+1)+repeat)]);
+		}
+	}
 
     elapsed_time = get_elapsed_time();
 
